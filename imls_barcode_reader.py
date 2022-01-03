9bba41e56d802b93ecfa352f622b5e2846372e58
dmarkbreiter <dmarkbreiter@nhm.org>
2022-01-03 13:53:06 -0800

    First commit

--- imls_barcode_reader.py
+++ imls_barcode_reader.py
@@ -15,12 +15,11 @@ def get_date():
     return today.strftime('%Y-%m-%d')
 
 class Ledger:
+    """Keeps track of letter order for filename and returns propoerly formmatted filename"""
     @staticmethod
     def number_to_letter():
-        letters = string.ascii_lowercase # Returns a string of all letters (lowercase to uppercase)
-        # Creates a dictionary of letters and their numerical order as values by zipping together the string of
-        # letters and a list of their numerical order. The list comprehension gets a list of order by taking the
-        # modulus of ascii_letter order (ord() function).
+        """Returns a dict of all letters and their numerical order"""
+        letters = string.ascii_lowercase
         number_translator = dict(zip([ord(letter) % 32 for letter in letters], letters))
         return number_translator
 
@@ -34,15 +33,24 @@ class Ledger:
         else:
             self.ledger[cat_number] = 1
 
-    def return_letter(self, cat_number):
+    def __return_letter(self, cat_number):
         self.__add_cat_number(cat_number)
         frequency = self.ledger[cat_number]
         return self.translator[frequency]
 
+    def return_filename(self, cat_number, taxon):
+        letter = self.__return_letter(cat_number)
+        cat_num = format_cat_number(cat_number)
+        file_name = f'LACMIP_{cat_num}_{taxon}_{letter}.jpg' if taxon else f'LACMIP_{cat_num}_{letter}.jpg'
+        return file_name
+
+
 
 class FilePaths:
+    """Class to contain all file paths"""
     def __init__(self, fp=None):
-        self.images = Path(fp) if fp else Path.cwd().parent # get the parent directory of the script where images are
+        # Get the parent directory of the script where images are, if no filepath is provided
+        self.images = Path(fp) if fp else Path.cwd().parent
         self.records = self.images.joinpath('PhotosRecord.csv')
         self.parent = self.images.joinpath(get_date())
         self.successes_parent = self.images.joinpath('successes')
@@ -59,7 +67,6 @@ class FilePaths:
         self.successes.mkdir(parents=True, exist_ok=True)
         self.failures.mkdir(parents=True, exist_ok=True)
 
-
     def save_to_records(self, record_dict):
         # If no photos were processed, do not write to the csv file
         if record_dict["TOTAL"] == 0:
@@ -74,7 +81,7 @@ class FilePaths:
 
 
 class Taxonomy:
-    fields = ['catalogNumber', 'kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'specificEpithet', 'taxonRank']
+    fields = ['catalogNumber', 'scientificName', 'irn']
 
     def __init__(self):
         self.df = pd.read_csv('taxonomy.csv', usecols=self.fields)
@@ -82,17 +89,8 @@ class Taxonomy:
     def return_taxon(self, cat_num):
         row = self.df.loc[self.df.catalogNumber == cat_num]
         if not row.empty:
-            kingdom = row['kingdom'].values[0]
-            taxon_rank = row['taxonRank'].values[0]
-            taxon_rank = taxon_rank.replace('Sub', '')
-            if taxon_rank == 'Species':
-                genus = row['genus'].values[0]
-                species = row['specificEpithet'].values[0]
-                taxon = f'{genus}_{species}'
-            elif taxon_rank == 'Kingdom' and kingdom == 'Unknown':
-                taxon = None
-            else:
-                taxon = row[taxon_rank.lower()].values[0]
+            scientific_name = row['scientificName'].values[0]
+            taxon = scientific_name if scientific_name else None
         else:
             taxon = None
         return taxon
@@ -102,86 +100,72 @@ def format_cat_number(number):
     return number.replace('.', '-')
 
 
-def sharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
-    """Return a sharpened version of the image, using an unsharp mask."""
-    blurred = cv2.GaussianBlur(image, kernel_size, sigma)
-    sharpened = float(amount + 1) * image - float(amount) * blurred
-    sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
-    sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
-    sharpened = sharpened.round().astype(np.uint8)
-    if threshold > 0:
-        low_contrast_mask = np.absolute(image - blurred) < threshold
-        np.copyto(sharpened, image, where=low_contrast_mask)
-    return sharpened
+class Image:
+    """Reads in an image and provides processing with cv2"""
+    def __init__(self, fp):
+        self.image = cv2.imread(str(fp), 0)
+        self.original_image = cv2.imread(str(fp), 0)
 
+    def sharpen(self, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
+        """Return a sharpened version of the image, using an unsharp mask."""
+        blurred = cv2.GaussianBlur(self.image, kernel_size, sigma)
+        sharpened = float(amount + 1) * self.image - float(amount) * blurred
+        sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+        sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+        sharpened = sharpened.round().astype(np.uint8)
+        if threshold > 0:
+            low_contrast_mask = np.absolute(self.image - blurred) < threshold
+            np.copyto(sharpened, self.image, where=low_contrast_mask)
+        self.image = sharpened
 
-def apply_brightness_contrast(input_img, brightness=0, contrast=0):
-    """Return an image with increased/decreased brightness and/or contrast"""
-    if brightness != 0:
-        if brightness > 0:
-            shadow = brightness
-            highlight = 255
+    def apply_brightness_contrast(self, brightness=0, contrast=0):
+        """Return an image with increased/decreased brightness and/or contrast"""
+        if brightness != 0:
+            if brightness > 0:
+                shadow = brightness
+                highlight = 255
+            else:
+                shadow = 0
+                highlight = 255 + brightness
+            alpha_b = (highlight - shadow) / 255
+            gamma_b = shadow
+
+            buf = cv2.addWeighted(self.image, alpha_b, self.image, 0, gamma_b)
         else:
-            shadow = 0
-            highlight = 255 + brightness
-        alpha_b = (highlight - shadow) / 255
-        gamma_b = shadow
+            buf = self.image.copy()
 
-        buf = cv2.addWeighted(input_img, alpha_b, input_img, 0, gamma_b)
-    else:
-        buf = input_img.copy()
+        if contrast != 0:
+            f = 131 * (contrast + 127) / (127 * (131 - contrast))
+            alpha_c = f
+            gamma_c = 127 * (1 - f)
 
-    if contrast != 0:
-        f = 131 * (contrast + 127) / (127 * (131 - contrast))
-        alpha_c = f
-        gamma_c = 127 * (1 - f)
+            buf = cv2.addWeighted(buf, alpha_c, buf, 0, gamma_c)
 
-        buf = cv2.addWeighted(buf, alpha_c, buf, 0, gamma_c)
+        self.image = buf
 
-    return buf
+    def binarize(self):
+        ret, self.image = cv2.threshold(self.image, 127, 255, cv2.THRESH_BINARY)
+
+    def read_barcode(self):
+        self.binarize()
+        self.sharpen(amount=2)
+        barcodes = pyzbar.decode(self.image)
+        return barcodes
 
 
-def print_barcode(dir=None):
+
+def main(dir=None):
     """Iterate over directory and detect all barcodes in .jpg images and print the barcode data"""
     ledger = Ledger()
     paths = FilePaths(fp=dir)
     taxonomy = Taxonomy()
     start = time.time()
-    taxonomy_df = pd.read_csv('taxonomy.csv', usecols=['catalogNumber',
-                                                       'kingdom',
-                                                       'phylum',
-                                                       'class',
-                                                       'order',
-                                                       'family',
-                                                       'genus',
-                                                       'specificEpithet',
-                                                       'taxonRank'])
+    taxonomy_df = pd.read_csv('taxonomy.csv')
     stats = {'SUCCESSES': 0, 'FAILURES': 0, 'TOTAL': 0, 'DATE': get_date()}
     for fp in paths.images.iterdir():
         if fp.suffix in ['.jpg', '.jpeg', '.JPG', '.jpeg']:
-            # Use opencv to read in image
-            image = cv2.imread(str(fp),0)  # The '0' parameter reads image in greyscale
-            original_image = cv2.imread(str(fp))
-            ret, image = cv2.threshold(image,127,255,cv2.THRESH_BINARY)
-            image = sharp_mask(image, amount=2)
-            '''
-            #image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE) # Rotate image
-            image = unsharp_mask(image)
-
-
-            image = apply_brightness_contrast(image, brightness=0, contrast=50)
-            #blur = cv2.GaussianBlur(image, (5, 5), 0)
-
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            se = cv2.getStructuringElement(cv2.MORPH_RECT, (8, 8))
-            bg = cv2.morphologyEx(image, cv2.MORPH_DILATE, se)
-            out_gray = cv2.divide(image, bg, scale=255)
-            #image = cv2.threshold(out_gray, 0, 255, cv2.THRESH_OTSU)[1]
-            '''
-
-            # Finds all barcodes in image
-            barcodes = pyzbar.decode(image)
-
+            image = Image(fp)
+            barcodes = image.read_barcode()
             if barcodes:
                 barcode_values = [b.data.decode('utf-8') for b in barcodes]
                 if 'LABELS' in barcode_values:
@@ -192,28 +176,25 @@ def print_barcode(dir=None):
                     taxon = taxonomy.return_taxon(f'LACMIP {barcode_values[0]}')
                     barcode_value = barcode_values[0]
                 print(f'{fp.stem}: {barcode_value}')
-                letter = ledger.return_letter(barcode_value)
+                #letter = ledger.return_letter(barcode_value)
                 cat_num = format_cat_number(barcode_value)
                 stats['SUCCESSES'] += 1
                 stats["TOTAL"] += 1
-                file_name = f'LACMIP_{cat_num}_{taxon}_{letter}.jpg' if taxon else f'LACMIP_{cat_num}_{letter}.jpg'
+                file_name = ledger.return_filename(cat_num, taxon)
+                #file_name = f'LACMIP_{cat_num}_{taxon}_{letter}.jpg' if taxon else f'LACMIP_{cat_num}_{letter}.jpg'
                 save_path = paths.successes.joinpath(file_name)
-                cv2.imwrite(str(save_path), image)
+                cv2.imwrite(str(save_path), image.original_image)
             else:
                 print(f'{fp.stem}: Null')
                 stats['FAILURES'] += 1
                 stats["TOTAL"] += 1
                 failure_date = get_date()
                 save_path = paths.failures.joinpath(f'{fp.stem}_{failure_date}_LACMIP_loc-lot_a.jpg')
-                cv2.imwrite(str(save_path), original_image)
+                cv2.imwrite(str(save_path), image.original_image)
             shutil.move(str(fp), str(paths.unnamed))
     print(f'{stats["SUCCESSES"]} successes \n{stats["FAILURES"]} failures.\
     \nThis function took {round(time.time()-start)} seconds')
     paths.save_to_records(stats)
 
 
-
-print_barcode(
-    dir=r'/Volumes/GoogleDrive/Shared drives/LACMIP Imaging/IMLS Type Specimens/2021-08-26 IMLS Barcode test for Daniel'
-)
-
+main(dir='/Volumes/GoogleDrive/Shared drives/LACMIP Imaging/IMLS Type Specimens/TO PROCESS')
